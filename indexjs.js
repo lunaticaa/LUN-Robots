dea37002bca074541c222edf39aea5b3e9aa2349
amir830047agh@gmail.com <amir830047agh@gmail.com>
2024-03-25 22:51:27 +0330

    adding style and create function for stories part and suggestion part

--- indexjs.js
+++ indexjs.js
@@ -1,7 +1,7 @@
 // mobile menu
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
-const hlink = document.querySelector("#hlink");
+const hlink = document.querySelectorAll("#hlink");
 const faSolid = document.querySelector(".fa-solid");
 
 hamburger.addEventListener("click", () => {
@@ -13,6 +13,23 @@ hlink.forEach(link => {
   link.addEventListener("click", () => {
     menu.classList.toggle("hidden");
     faSolid.classList.toggle("fa-xmark");
-  })
+  });
+});
 
-})
+// story Image of products
+
+const goodsTexts = document.getElementsByClassName("goods-text");
+const goodsPics = document.getElementsByClassName("goods-pic");
+
+function showImg(){
+  for(goodsPic of goodsPics){
+    goodsPic.classList.remove("active-pic");
+  }
+
+  for(goodstext of goodsTexts){
+    goodstext.classList.remove("active-text");
+  }
+  let i = Array.from(goodsPics).indexOf(event.target);
+  goodsPics[i].classList.add('active-pic');
+  goodsTexts[i].classList.add('active-text');
+};
