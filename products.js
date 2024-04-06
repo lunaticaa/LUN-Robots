b41ed46f03c3ff5d41f1787078b9fb3be3f5ef36
amir830047agh@gmail.com <amir830047agh@gmail.com>
2024-04-06 15:00:59 +0330

    list and name and price of products

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+      "id": 1,
+      "name":" labtab3",
+      "price": 200,
+      "image": "imgs/lab1.jpg",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 2,
+      "name":" robot 4",
+      "price": 250,
+      "image": "imgs/lab3.jpg",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 3,
+      "name":" graphic card 3",
+      "price": 290,
+      "image": "imgs/lab 2.png",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 4,
+      "name":" graphc card 2",
+      "price": 200,
+      "image": "imgs/graphic card2.webp",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 5,
+      "name":" robot1",
+      "price": 300,
+      "image": "imgs/robot-gr.png",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 6,
+      "name":"labtab 1",
+      "price": 200,
+      "image": "imgs/graphic card 3.webp",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 7,
+      "name":"robot2",
+      "price": 200,
+      "image": "imgs/best card graphic.webp",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  },
+  {
+      "id": 8,
+      "name":"graphic card1",
+      "price": 200,
+      "image": "imgs/graphic card.webp",
+      "description": "these are some of my products that can help you to make a robot with your own skills like what i have done before and become like amirmohammad so smart and brilliant"
+  }
+
+];
+export default products;
